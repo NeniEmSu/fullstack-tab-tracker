d48482db9d4ab18928072ef79d7e2eceaef00770
cryptodoctor <emmanuelneni@gmail.com>
2020-05-09 17:18:53 +0300

    initialized server for accessing routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
+/* eslint-disable no-console */
+const path = require('path')
 const express = require('express')
 const consola = require('consola')
+const morgan = require('morgan')
+// const cors = require('cors')
 const { Nuxt, Builder } = require('nuxt')
 const app = express()
+const bodyParser = require('body-parser')
+const config = require('../nuxt.config.js')
+require('dotenv').config()
 
 // Import and Set Nuxt.js options
-const config = require('../nuxt.config.js')
 config.dev = process.env.NODE_ENV !== 'production'
 
 async function start() {
@@ -19,6 +25,35 @@ async function start() {
     const builder = new Builder(nuxt)
     await builder.build()
   }
+
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization'
+    )
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET, POST, PUT, DELETE, PATCH, OPTIONS'
+    )
+    next()
+  })
+
+  // app.use(cors())
+  // configure body parser
+  app.use(bodyParser.urlencoded({ extended: false }))
+  app.use(bodyParser.json())
+
+  app.use(morgan('dev')) // configire morgan
+
+  app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
+
+  // Endpoints
+  app.get('/status', (req, res) => {
+    res.send({
+      message: 'working without cors'
+    })
+  })
 
   // Give nuxt middleware to express
   app.use(nuxt.render)
